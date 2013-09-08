f7152c6659c28c07b32be47c526565edb7b07526
Anthonyweng <739200964@qq.com>
2013-09-08 23:19:02 +0800

    Anthonyweng

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication5.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(int argc, _TCHAR* argv[])
+{
+	cout<<"Hello World! I'm 翁嘉骏 2012060050011";
+	return 0;
+}
+
